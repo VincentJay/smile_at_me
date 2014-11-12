108e484994c6f065bcc3cb31fcbeecf5514bd055
Vincent Jay <jixiangcheng1@163.com>
2014-11-12 19:51:02 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SmileAtMe::Application.config.secret_key_base = '893bee63ff17d0691d1c783072479624df5d7e45b22ae906f8cfe670e25437016f7c4bea247ef5695ab3c5000130ce4265bddd12a959db0038a32ed68c48836e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SmileAtMe::Application.config.session_store :cookie_store, key: '_smile_at_me_session'
